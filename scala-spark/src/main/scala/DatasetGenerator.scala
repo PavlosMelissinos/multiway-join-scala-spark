c74df02e95143b3e7b3aaffefd857d377e5d454d
guardianhelm <guardianhelm@gmail.com>
2015-12-04 22:01:54 +0200

    Complete dataset generator class
    Generates lines that match either one of:
    * a, <int>, <string>
    * b, <int>, <string>
    * c, <int>, <string>
    * r, <int>, <int>, <int>, <string>
    
    Also saves to file

--- scala-spark/src/main/scala/DatasetGenerator.scala
+++ scala-spark/src/main/scala/DatasetGenerator.scala
@@ -1,3 +1,5 @@
+import java.io.{File, PrintWriter}
+
 /**
  * Created by ThirstyTM on 2015-11-21.
  */
@@ -6,7 +8,7 @@ class DatasetGenerator(aSize: Int, bSize: Int, cSize: Int, rSize: Int) {
   val as = for(i <- 1 to aSize) yield generateWithPrimaryKey('A', i)
   val bs = for(i <- 1 to bSize) yield generateWithPrimaryKey('B', i)
   val cs = for(i <- 1 to cSize) yield generateWithPrimaryKey('C', i)
-  val rs = for(i <- 1 to rSize) yield genR(as.size, bs.size, cs.size) //TODO: remove duplicates??
+  val rs = for(i <- 1 to rSize) yield genR //TODO: remove duplicates??
 
   private def genR(id: Int) = {
     val rRow = List()
@@ -22,13 +24,28 @@ class DatasetGenerator(aSize: Int, bSize: Int, cSize: Int, rSize: Int) {
     (rel, id, genString)
   }
 
-  private def genR(aSize: Int, bSize: Int, cSize: Int) ={
+//  private def genR(aSize: Int, bSize: Int, cSize: Int) ={
+  private def genR = {
     val R = 'R'
-    val a = random.nextInt(aSize)
-    val b = random.nextInt(bSize)
-    val c = random.nextInt(cSize)
+    val a = random nextInt as.size
+    val b = random nextInt bs.size
+    val c = random nextInt cs.size
     val value = genString
-    (R, a, b, c, value)
+    (R, a + 1, b + 1, c + 1, value)
   }
 
+  private def tuple2str(tuple: Product): String ={
+    tuple.productIterator.toList.mkString(",")
+  }
+
+  def serialize(filename: String) = {
+//    val filename = "D:\\Programming\\repos\\pms-ddm\\scala-spark\\dataset.txt"
+    val writer = new PrintWriter(new File(filename))
+    as map tuple2str foreach (writer.println)
+    as map tuple2str foreach (println)
+    bs map tuple2str foreach (writer.println)
+    cs map tuple2str foreach (writer.println)
+    rs map tuple2str foreach (writer.println)
+    writer close
+  }
 }
