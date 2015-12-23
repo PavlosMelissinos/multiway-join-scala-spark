cca8b4a92c099a0b0c91e00726cf787ece27c137
guardianhelm <guardianhelm@gmail.com>
2015-12-23 20:12:15 +0200

    Edit/cleanup for final deliverable
    star join not working properly

--- scala-spark/src/main/scala/nonFinal/master-ddm.scala
+++ scala-spark/src/main/scala/nonFinal/master-ddm.scala
@@ -23,70 +23,64 @@ class SparkJoin(dataset: String){
   val sc = new SparkContext(sparkConf(cores))
   val records = (sc textFile dataset).map(_ split ",")
 
-  val relR = records.filter(p => p(0) equals "R")
-  val relA = records.filter(p => p(0) equals "A")
-  val relB = records.filter(p => p(0) equals "B")
-  val relC = records.filter(p => p(0) equals "C")
+  //split relations by type
+  val rel = (relType: String) => records.filter(p => p(0) equals relType)
 
   def sqlJoin : DataFrame = {
-//    val sc = new SparkContext(sparkConf(2))
     val sqlContext = new SQLContext(sc)
     // this is used to implicitly convert an RDD to a DataFrame.
     import sqlContext.implicits._
 
     // Create RDDs for each relation and register all of them as tables.
-    val relRFinal = relR.map(p => RecordR(p(1).trim.toInt, p(2).trim.toInt, p(3).trim.toInt, p(4).trim.toInt)).toDF()
+    val relRFinal = rel("R").map(p => RecordR(p(1).trim.toInt, p(2).trim.toInt, p(3).trim.toInt, p(4).trim.toInt)).toDF()
     relRFinal registerTempTable "R"
 
-    val relAFinal = relA.map(p => RecordA(p(1).trim.toInt, p(2))).toDF()
+    val relAFinal = rel("A").map(p => RecordA(p(1).trim.toInt, p(2))).toDF()
     relAFinal registerTempTable "A"
 
-    val relBFinal = relB.map(p => RecordB(p(1).trim.toInt, p(2))).toDF()
+    val relBFinal = rel("B").map(p => RecordB(p(1).trim.toInt, p(2))).toDF()
     relBFinal registerTempTable "B"
 
-    val relCFinal = relC.map(p => RecordC(p(1).trim.toInt, p(2))).toDF()
+    val relCFinal = rel("C").map(p => RecordC(p(1).trim.toInt, p(2))).toDF()
     relCFinal registerTempTable "C"
 
     val joinSQL = sqlContext.sql("SELECT R.a, R.b, R.c, R.value, A.x, B.y, C.z FROM R, A, B, C WHERE R.a = A.a AND R.b = B.b AND R.c = C.c")
     joinSQL
-//    joinSQL foreach println
-//    val jsqlrdd = joinSQL.rdd
-//    jsqlrdd foreach println
-//    jsqlrdd
   }
 
   //Performs "chained" join in the following order ((R x A) x B) x C
   def join: RDD[(Int, Int, Int, Int, String, String, String)] = {
-
-    val relRFinal = relR.map(r => (r(1).trim.toInt, (r(1).trim.toInt, r(2).trim.toInt, r(3).trim.toInt, r(4).trim.toInt))) //['R', a, b, c, value] to (a, [a, b, c, value])
-
-    val relAFinal = relA.map(a => (a(1).trim.toInt, a(2))) //['A', a, x] to (a, x)
+    val relRFinal = rel("R").map(r => (r(1).trim.toInt, (r(1).trim.toInt, r(2).trim.toInt, r(3).trim.toInt, r(4).trim.toInt))) //['R', a, b, c, value] to (a, [a, b, c, value])
+    val relAFinal = rel("A").map(a => (a(1).trim.toInt, a(2))) //['A', a, x] to (a, x)
+    val relBFinal = rel("B").map(b => (b(1).trim.toInt, b(2))) //['B', b, y] to (b, y)
+    val relCFinal = rel("C").map(c => (c(1).trim.toInt, c(2))) //['C', c, z] to (c, z)
 
     // join relR and relA so that the result will be (a, ([a, b, c, value], x))
     // then transform result to (b, [a, b, c, value, x])
     val relRxA = relRFinal.join(relAFinal)
       .map{case(key, ((a, b, c, value), x)) => (b, (a, b, c, value, x))}
-
-    val relBFinal = relB.map(b => (b(1).trim.toInt, b(2))) //['B', b, y] to (b, y)
-
     // join relRxA and relB so the result will be: (b, ([a, b, c, value, x], y))
     // then transform result to (c, [a, b, c, value, x, y])
     val relRxAxB = relRxA.join(relBFinal)
       .map{case(key, ((a, b, c, value, x), y)) => (c, (a, b, c, value, x, y))}
-
-    val relCFinal = relC.map(c => (c(1).trim.toInt, c(2))) //['C', c, z] to (c, z)
-
     // join relRxAxB and relC so the result will be: (c, ([a, b, c, value, x, y], z))
     // then transform result to [a, b, c, value, x, y, z])
-    val relRxAxBxC = relRxAxB.join(relCFinal)
-      .map{case(key, ((a, b, c, value, x, y), z)) => (a, b, c, value, x, y, z)}
-
-//    relRxAxBxC.foreach(println)
-    relRxAxBxC
+    relRxAxB.join(relCFinal).map{case(key, ((a, b, c, value, x, y), z)) => (a, b, c, value, x, y, z)}
   }
 
+  def assignAttrShares(reducers: Int): (Int, Int, Int) ={
+    val d1 = rel("A").count()
+    val d2 = rel("B").count()
+    val d3 = rel("C").count()
 
-  //    : RDD[Array[String]]
+    //compute attribute shares a,b,c according to the paper's instructions
+    val k = reducers // = a * b * c
+    val a = Math.cbrt(k * d1 * d1 / (d2 * d3)).toInt
+    val b = Math.cbrt(k * d2 * d2 / (d1 * d3)).toInt
+    val c = Math.cbrt(k * d3 * d3 / (d1 * d2)).toInt
+    (a, b, c)
+  }
+
   /**
     * a = sqrt3(k * d1 * d1 / (d2 * d3))
     * b = sqrt3(k * d2 * d2 / (d1 * d3))
@@ -95,78 +89,63 @@ class SparkJoin(dataset: String){
     */
   def starJoin (reducers: Int) = {
 
-    val d1 = relA.count()
-    val d2 = relB.count()
-    val d3 = relC.count()
-
     val currentDir = System.getProperty("user.dir") // get the current directory
     System.setProperty("hadoop.home.dir", currentDir)
-
-    //compute attribute shares a,b,c according to the paper's instructions
-    val k = reducers // = a * b * c
-    val a = Math.cbrt(k * d1 * d1 / (d2 * d3)).toInt
-    val b = Math.cbrt(k * d2 * d2 / (d1 * d3)).toInt
-    val c = Math.cbrt(k * d3 * d3 / (d1 * d2)).toInt
+    val (a, b, c) = assignAttrShares(reducers: Int)
 
     //hash functions matching each record value to a partial mapkey
-    val hA = (n: String) => 1 + (n.trim.toInt % a)
-    val hB = (n: String) => 1 + (n.trim.toInt % b)
-    val hC = (n: String) => 1 + (n.trim.toInt % c)
+    val hashFun = (n: Int, sz: Int) => n % sz + 1
+    val hA = (n: Int) => hashFun(n, a)
+    val hB = (n: Int) => hashFun(n, b)
+    val hC = (n: Int) => hashFun(n, c)
 
     //mapper: assigns a unique mapkey to each record
     val mapFun: (Array[String] => Seq[((Int, Int, Int), (String, String))]) = (sArr: Array[String]) => {
       sArr(0) match{
         case "R" => {
-//          val recR = rec.asInstanceOf[RecordR]
           val recR = RecordR(sArr(1).trim.toInt, sArr(2).trim.toInt, sArr(3).trim.toInt, sArr(4).trim.toInt)
           val sValue = sArr(1) + "," + sArr(2) + "," + sArr(3) + "," + sArr(4)
-          Seq(((recR.a % a + 1, recR.b % b + 1, recR.c % c + 1), (sValue, "R")))
+          Seq(((hA(recR.a), hB(recR.b), hC(recR.c)), (sValue, "R")))
         }
         case "A" => {
           val recA = RecordA(sArr(1).trim.toInt, sArr(2).trim)
-          val sValue = sArr(1) + "," + sArr(2)
-          val res = for {
+          val sValue = sArr(2)
+          for {
             j <- 1 to b;
             k <- 1 to c
-          } yield ((recA.a % a + 1, j, k), (sValue, "A"))
-          res
+          } yield ((hA(recA.a), j, k), (sValue, "A"))
         }
         case "B" => {
           val recB = RecordB(sArr(1).trim.toInt, sArr(2).trim)
-          val sValue = sArr(1) + "," + sArr(2)
-          val res = for {
+          val sValue = sArr(2)
+          for {
             i <- 1 to a;
             k <- 1 to c
-          } yield ((i, recB.b % b + 1, k), (sValue, "B"))
-          res
+          } yield ((i, hB(recB.b), k), (sValue, "B"))
         }
         case "C" => {
           val recC = RecordC(sArr(1).trim.toInt, sArr(2).trim)
-          val sValue = sArr(1) + "," + sArr(2)
-          val res = for {
+          val sValue = sArr(2)
+          for {
             i <- 1 to a;
             j <- 1 to b
-          } yield ((i, j, recC.c % c + 1), (sValue, "C"))
-          res
+          } yield ((i, j, hC(recC.c)), (sValue, "C"))
         }
       }
     }
 
     val redFun = (a: (String, String), b: (String, String)) => {
-      if (a._2.equals(b._2))
+      if (a._2.contains(b._2))
         (a._1, a._2)
+      else if (b._2.contains(a._2))
+        (b._1, b._2)
       else
         (a._1 + "," + b._1, "(" + a._2 + "x" + b._2 + ")")
-//        (a._1 + "," + b._1, "(" + a._2 + "x" + b._2 + ")")
     }
 
-    val mapped = records.flatMap(mapFun)
-//    mapped.foreach(println)
+    val mapped = records.flatMap(mapFun) //assigns a mapkey to each record
     val saveDir = Array(System.getProperty("user.dir"), "output", "starJoinMapped").mkString(java.io.File.separator)
     mapped.saveAsTextFile(saveDir)
-//    val partitioned = mapped.partitionBy()
-    val res = mapped.reduceByKey((a, b) => redFun(a, b))
-    res
-//    res.values.map(a => a._1)
+    mapped.reduceByKey((a, b) => redFun(a, b))
   }
 }
--- scala-spark/src/main/scala/nonFinal/spark-test.scala
+++ scala-spark/src/main/scala/nonFinal/spark-test.scala
@@ -1,40 +1,30 @@
-import nonFinal.StarJoin
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkConf
-
 object EvalAll{
   def time(f: => Unit)={
     val s = System.currentTimeMillis
     f
     System.currentTimeMillis - s
   }
+  def sqlJoin(sj: SparkJoin, saveDir:String): Unit = {
+    val res = sj.sqlJoin //run sql join
+    res.write.save(saveDir)
+  }
+  def sparkJoin(sj: SparkJoin, saveDir:String): Unit = {
+    val res = sj.join //run spark join
+    res.saveAsTextFile(saveDir)
+  }
+  def starJoin(sj: SparkJoin, saveDir:String, reducers: Int): Unit = {
+    val res = sj.starJoin(reducers)
+    res.saveAsTextFile(saveDir)
+  }
+
   def main(args: Array[String]): Unit = {
     val reducers = if (args.size > 0) args(0).trim.toInt else 64
     val inputFile = if (args.size > 1) args(1) else Array(System.getProperty("user.dir"), "data", "dataset.txt").mkString(java.io.File.separator)
     val outputFile = if (args.size > 2) args(2) else Array(System.getProperty("user.dir"), "output").mkString(java.io.File.separator)
 
     val sj = new SparkJoin(inputFile)
-//    println("Spark join, time:" + time(sparkJoin(sj, outputFile + java.io.File.separator + "sparkJoin")))
-
-//    println("Sql join, time:" + time(sqlJoin(sj, outputFile + java.io.File.separator + "sqlJoin")))
-
-    println("Star join," + reducers + " reducers, time:" + time(starJoin(sj, outputFile + java.io.File.separator + "starJoin", reducers)))
-  }
-
-  def sqlJoin(sj: SparkJoin, saveDir:String): Unit = {
-    val res = sj.sqlJoin //run sql join
-    res.foreach(println)
-    res.write.save(saveDir)
-  }
-
-  def sparkJoin(sj: SparkJoin, saveDir:String): Unit = {
-    val res = sj.join //run spark join
-    res.saveAsTextFile(saveDir)
-  }
-
-  def starJoin(sj: SparkJoin, saveDir:String, reducers: Int): Unit = {
-    val res = sj.starJoin(reducers)
-    res.saveAsTextFile(saveDir)
-//    println(res._2)
+    println("Spark join, time:" + time(sparkJoin(sj, outputFile + java.io.File.separator + "sparkJoin")))
+    println("Sql join, time:" + time(sqlJoin(sj, outputFile + java.io.File.separator + "sqlJoin")))
+    println("Star join, " + reducers + " reducers, time:" + time(starJoin(sj, outputFile + java.io.File.separator + "starJoin", reducers)))
   }
 }
